c9574043e295f635bbddd834da5fd39d09200646
JoshGonzalez <56143006+JoshGonzalez@users.noreply.github.com>
2020-02-17 15:31:34 -0800

    Add files via upload

--- ASCII.c
+++ ASCII.c
@@ -1,21 +1,23 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define BINLEN 8
+char* Dec_to_Bin(int dec);
+char* Base_to_Bin(int dec, int basenum){
+  static char bin[BINLEN];
+  size_t i = 0;
+  memset(bin, '0', BINLEN);
+ while(dec > 0){
+   size_t rem = dec % basenum;
+   if(rem == 0){bin[i++] = '0';}
+   //printf("Remainder is %d\n", bin[i]);
+   else{bin[i++] = (rem + '0');}
+   dec = dec / basenum;
+   //printf("New number is %d\n", dec);
+ };
 
-int* Dec_to_Bin(int dec);
-
-int* Dec_to_Bin(int dec){
-  int bin[8];
-  int i = 8;
-  while(dec > 0){
-    bin[i] = dec % 2;
-    //printf("%d\n", bin[i]);
-    dec = dec / 2;
-    //printf("%d\n", dec);
-    --i;
-  }
-
-  return bin;
+ return bin;
 }
 
 int main(int argc, const char *argv[]){
@@ -25,25 +27,26 @@ int main(int argc, const char *argv[]){
 FILE *fout;
 
 if(argc != 2){
-  fprintf(stderr, "Usage: ./Chart fileoutput.txt\n");
-  exit(1);
+ fprintf(stderr, "Usage: ./Chart fileoutput.txt\n");
+ exit(1);
 }
 
 fout = fopen(argv[1], "w");
 
 if(fout == NULL){
-  fprintf(stderr, "Can not open output file %s\n", argv[1]);
-  exit(0);
+ fprintf(stderr, "Can not open output file %s\n", argv[1]);
+ exit(0);
 }
 
 fprintf(fout, "%3s || %8s\n", "Dec", "Bin");
 
-for(int i = 127; i >= 0; --i){
-  int *num = Dec_to_Bin(i);
-  fprintf(fout, "%03d || ", i);
-  for(int i = sizeof(num) - 1; i >= 0; --i){
-  fprintf(fout, "%d", *(num + i));
-  }
-  fprintf(fout, "\n");
+for(int i = 128; i >= 0; --i){
+ char *str = Base_to_Bin(i, 10);
+ //char *bin = Reverse(str);
+ fprintf(fout, "%03d ||", i);
+ for(int i = sizeof(str) - 1; i >= 0; --i){
+ fprintf(fout, "%c", str[i]);
+ }
+ fprintf(fout, "\n");
 }
 }
